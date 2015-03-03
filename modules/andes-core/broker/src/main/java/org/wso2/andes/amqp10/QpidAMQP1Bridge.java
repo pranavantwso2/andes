ad6e622cd3d1ba08568976305eab7320ebe4b98a
megala <ktumegala@yahoo.com>
2015-03-03 10:02:57 +0530

    Qpid Protocol Engine New version added

---
+++ modules/andes-core/broker/src/main/java/org/wso2/andes/amqp10/QpidAMQP1Bridge.java
@@ -0,0 +1,16 @@
+package org.wso2.andes.amqp10;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * This is the mediation point between Qpid and AMQP1.0.
+ * Any interaction should be mediated through this bridge
+ */
+public class QpidAMQP1Bridge {
+    private static Log log = LogFactory.getLog(QpidAMQP1Bridge.class);
+    private static QpidAMQP1Bridge qpidAMQPBridge = null;
+
+
+
+}
