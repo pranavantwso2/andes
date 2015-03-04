7f23b56a85967b0c18e1cdd8ce82e5fe863d80b2
megala <ktumegala@yahoo.com>
2015-03-04 09:36:28 +0530

    Bridge for the AMQP1

--- modules/andes-core/broker/src/main/java/org/wso2/andes/amqp10/QpidAMQP1Bridge.java
+++ modules/andes-core/broker/src/main/java/org/wso2/andes/amqp10/QpidAMQP1Bridge.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.wso2.andes.amqp10;
 
 import org.apache.commons.logging.Log;
@@ -9,7 +27,19 @@ import org.apache.commons.logging.LogFactory;
  */
 public class QpidAMQP1Bridge {
     private static Log log = LogFactory.getLog(QpidAMQP1Bridge.class);
-    private static QpidAMQP1Bridge qpidAMQPBridge = null;
+    private static QpidAMQP1Bridge qpidAMQP1Bridge = null;
+
+    /**
+     * get QpidAMQPBridge instance
+     *
+     * @return QpidAMQPBridge instance
+     */
+    public static synchronized QpidAMQP1Bridge getInstance() {
+        if (qpidAMQP1Bridge == null) {
+            qpidAMQP1Bridge = new QpidAMQP1Bridge();
+        }
+        return qpidAMQP1Bridge;
+    }
 
 
 
